85d52c7d8c8edec9c39987447aca198150ee3973
cordmaximilian <71908512+cordmaximilian@users.noreply.github.com>
2025-06-27 00:41:38 +0200

    fix dbt cycles by renaming staging models

--- dbt/models/silver/wheat_weather.sql
+++ dbt/models/silver/wheat_weather.sql
@@ -4,7 +4,7 @@ select
     cp.timestamp,
     cp.price as wheat_price,
     w.temperature_c
-from {{ ref('commodity_prices') }} as cp
-left join {{ ref('weather') }} as w
+from {{ ref('stg_commodity_prices') }} as cp
+left join {{ ref('stg_weather') }} as w
   on cp.timestamp = w.timestamp
 where cp.commodity = 'wheat'
